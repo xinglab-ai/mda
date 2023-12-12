11b87526a6b77e240110e6c4fb4eeaed27990f9c
tauhidstanford <52015296+tauhidstanford@users.noreply.github.com>
2023-12-12 13:59:14 -0800

    Add files via upload

---
+++ example_pytorch_feature_extraction.py
@@ -0,0 +1,320 @@
+# This code shows the process of extracting features from a PyTorch model
+# developed by Meta researchers
+# For details about the model or paper, see https://github.com/facebookresearch/ConvNeXt
+# https://arxiv.org/abs/2201.03545
+
+
+# Ref: Islam et al, Revealing Hidden Patterns in Deep Neural Network Feature Space Continuum 
+# via Manifold Learning, in press, Nature Communications, 2023
+
+import argparse
+import datetime
+import numpy as np
+import time
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import json
+import torch.distributed as dist
+import os
+
+os.environ['MASTER_ADDR'] = 'localhost'
+os.environ['MASTER_PORT'] = '12345'
+
+from pathlib import Path
+
+from timm.data.mixup import Mixup
+from timm.models import create_model
+from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
+from timm.utils import ModelEma
+from optim_factory import create_optimizer, LayerDecayValueAssigner
+
+from datasets import build_dataset
+from engine import train_one_epoch, evaluate
+
+from utils import NativeScalerWithGradNormCount as NativeScaler
+import utils
+import models.convnext
+import models.convnext_isotropic
+
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
+import numpy as np
+
+
+
+def str2bool(v):
+    """
+    Converts string to bool type; enables command line 
+    arguments in the format of '--arg1 true --arg2 false'
+    """
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+        
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('ConvNeXt training and evaluation script for image classification', add_help=False)
+    parser.add_argument('--batch_size', default=64, type=int,
+                        help='Per GPU batch size')
+    parser.add_argument('--epochs', default=300, type=int)
+    parser.add_argument('--update_freq', default=1, type=int,
+                        help='gradient accumulation steps')
+
+    # Model parameters
+    parser.add_argument('--model', default='convnext_base', type=str, metavar='MODEL',
+                        help='Name of model to train')
+    parser.add_argument('--drop_path', type=float, default=0, metavar='PCT',
+                        help='Drop path rate (default: 0.0)')
+    parser.add_argument('--input_size', default=224, type=int,
+                        help='image input size')
+    parser.add_argument('--layer_scale_init_value', default=1e-6, type=float,
+                        help="Layer scale initial values")
+
+    # EMA related parameters
+    parser.add_argument('--model_ema', type=str2bool, default=False)
+    parser.add_argument('--model_ema_decay', type=float, default=0.9999, help='')
+    parser.add_argument('--model_ema_force_cpu', type=str2bool, default=False, help='')
+    parser.add_argument('--model_ema_eval', type=str2bool, default=False, help='Using ema to eval during training.')
+
+    # Optimization parameters
+    parser.add_argument('--opt', default='adamw', type=str, metavar='OPTIMIZER',
+                        help='Optimizer (default: "adamw"')
+    parser.add_argument('--opt_eps', default=1e-8, type=float, metavar='EPSILON',
+                        help='Optimizer Epsilon (default: 1e-8)')
+    parser.add_argument('--opt_betas', default=None, type=float, nargs='+', metavar='BETA',
+                        help='Optimizer Betas (default: None, use opt default)')
+    parser.add_argument('--clip_grad', type=float, default=None, metavar='NORM',
+                        help='Clip gradient norm (default: None, no clipping)')
+    parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                        help='SGD momentum (default: 0.9)')
+    parser.add_argument('--weight_decay', type=float, default=0.05,
+                        help='weight decay (default: 0.05)')
+    parser.add_argument('--weight_decay_end', type=float, default=None, help="""Final value of the
+        weight decay. We use a cosine schedule for WD and using a larger decay by
+        the end of training improves performance for ViTs.""")
+
+    parser.add_argument('--lr', type=float, default=4e-3, metavar='LR',
+                        help='learning rate (default: 4e-3), with total batch size 4096')
+    parser.add_argument('--layer_decay', type=float, default=1.0)
+    parser.add_argument('--min_lr', type=float, default=1e-6, metavar='LR',
+                        help='lower lr bound for cyclic schedulers that hit 0 (1e-6)')
+    parser.add_argument('--warmup_epochs', type=int, default=20, metavar='N',
+                        help='epochs to warmup LR, if scheduler supports')
+    parser.add_argument('--warmup_steps', type=int, default=-1, metavar='N',
+                        help='num of steps to warmup LR, will overload warmup_epochs if set > 0')
+
+    # Augmentation parameters
+    parser.add_argument('--color_jitter', type=float, default=0.4, metavar='PCT',
+                        help='Color jitter factor (default: 0.4)')
+    parser.add_argument('--aa', type=str, default='rand-m9-mstd0.5-inc1', metavar='NAME',
+                        help='Use AutoAugment policy. "v0" or "original". " + "(default: rand-m9-mstd0.5-inc1)'),
+    parser.add_argument('--smoothing', type=float, default=0.1,
+                        help='Label smoothing (default: 0.1)')
+    parser.add_argument('--train_interpolation', type=str, default='bicubic',
+                        help='Training interpolation (random, bilinear, bicubic default: "bicubic")')
+
+    # Evaluation parameters
+    parser.add_argument('--crop_pct', type=float, default=None)
+
+    # * Random Erase params
+    parser.add_argument('--reprob', type=float, default=0.25, metavar='PCT',
+                        help='Random erase prob (default: 0.25)')
+    parser.add_argument('--remode', type=str, default='pixel',
+                        help='Random erase mode (default: "pixel")')
+    parser.add_argument('--recount', type=int, default=1,
+                        help='Random erase count (default: 1)')
+    parser.add_argument('--resplit', type=str2bool, default=False,
+                        help='Do not random erase first (clean) augmentation split')
+
+    # * Mixup params
+    parser.add_argument('--mixup', type=float, default=0.8,
+                        help='mixup alpha, mixup enabled if > 0.')
+    parser.add_argument('--cutmix', type=float, default=1.0,
+                        help='cutmix alpha, cutmix enabled if > 0.')
+    parser.add_argument('--cutmix_minmax', type=float, nargs='+', default=None,
+                        help='cutmix min/max ratio, overrides alpha and enables cutmix if set (default: None)')
+    parser.add_argument('--mixup_prob', type=float, default=1.0,
+                        help='Probability of performing mixup or cutmix when either/both is enabled')
+    parser.add_argument('--mixup_switch_prob', type=float, default=0.5,
+                        help='Probability of switching to cutmix when both mixup and cutmix enabled')
+    parser.add_argument('--mixup_mode', type=str, default='batch',
+                        help='How to apply mixup/cutmix params. Per "batch", "pair", or "elem"')
+
+    # * Finetuning params
+    parser.add_argument('--finetune', default='',
+                        help='finetune from checkpoint')
+    parser.add_argument('--head_init_scale', default=1.0, type=float,
+                        help='classifier head initial scale, typically adjusted in fine-tuning')
+    parser.add_argument('--model_key', default='model|module', type=str,
+                        help='which key to load from saved state dict, usually model or model_ema')
+    parser.add_argument('--model_prefix', default='', type=str)
+
+    # Dataset parameters
+    parser.add_argument('--data_path', default='E:\imagenet_structured', type=str,
+                        help='dataset path')
+    parser.add_argument('--eval_data_path', default=None, type=str,
+                        help='dataset path for evaluation')
+    parser.add_argument('--nb_classes', default=1000, type=int,
+                        help='number of the classification types')
+    parser.add_argument('--imagenet_default_mean_and_std', type=str2bool, default=True)
+    parser.add_argument('--data_set', default='IMNET', choices=['CIFAR', 'IMNET', 'image_folder'],
+                        type=str, help='ImageNet dataset path')
+    parser.add_argument('--output_dir', default='',
+                        help='path where to save, empty for no saving')
+    parser.add_argument('--log_dir', default=None,
+                        help='path where to tensorboard log')
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
+    parser.add_argument('--seed', default=0, type=int)
+
+    parser.add_argument('--resume', default='',
+                        help='resume from checkpoint')
+    parser.add_argument('--auto_resume', type=str2bool, default=True)
+    parser.add_argument('--save_ckpt', type=str2bool, default=True)
+    parser.add_argument('--save_ckpt_freq', default=1, type=int)
+    parser.add_argument('--save_ckpt_num', default=3, type=int)
+
+    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+                        help='start epoch')
+    parser.add_argument('--eval', type=str2bool, default=False,
+                        help='Perform evaluation only')
+    parser.add_argument('--dist_eval', type=str2bool, default=True,
+                        help='Enabling distributed evaluation')
+    parser.add_argument('--disable_eval', type=str2bool, default=False,
+                        help='Disabling evaluation during training')
+    parser.add_argument('--num_workers', default=10, type=int)
+    parser.add_argument('--pin_mem', type=str2bool, default=True,
+                        help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+
+    # distributed training parameters
+    parser.add_argument('--world_size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--local_rank', default=-1, type=int)
+    parser.add_argument('--dist_on_itp', type=str2bool, default=False)
+    parser.add_argument('--dist_url', default='env://',
+                        help='url used to set up distributed training')
+    parser.add_argument('--backend', default='gloo',
+                        help='backend added by tauhid')
+
+    parser.add_argument('--use_amp', type=str2bool, default=False, 
+                        help="Use PyTorch's AMP (Automatic Mixed Precision) or not")
+
+    # Weights and Biases arguments
+    parser.add_argument('--enable_wandb', type=str2bool, default=False,
+                        help="enable logging to Weights and Biases")
+    parser.add_argument('--project', default='convnext', type=str,
+                        help="The name of the W&B project where you're sending the new run.")
+    parser.add_argument('--wandb_ckpt', type=str2bool, default=False,
+                        help="Save model checkpoints as W&B Artifacts.")
+
+    return parser
+        
+        
+parser = argparse.ArgumentParser('ConvNeXt training and evaluation script', parents=[get_args_parser()])
+args = parser.parse_args()
+
+# Load the trained model
+state_dict = torch.load('save/convnext_base_22k_1k_224.pth', map_location='cuda')
+
+# Adjust the keys if the model was trained using nn.DataParallel or nn.DistributedDataParallel
+new_state_dict = {}
+for k, v in state_dict.items():
+    name = k[7:] if k.startswith('module.') else k  # remove `module.` if present
+    new_state_dict[name] = v
+
+model = create_model(
+    args.model, 
+    pretrained=False, 
+    num_classes=args.nb_classes, 
+    drop_path_rate=args.drop_path,
+    layer_scale_init_value=args.layer_scale_init_value,
+    head_init_scale=args.head_init_scale,
+    )
+model.load_state_dict(new_state_dict['model'])
+
+device=torch.device('cuda')
+model.to(device)
+
+
+
+# Define your transformations
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    # Add other necessary transformations
+])
+
+# Load the dataset
+full_dataset = datasets.ImageFolder('E:/imagenet_structured/val_sub', transform=transform)
+
+# Create a DataLoader for the subset
+subset_loader = DataLoader(full_dataset, batch_size=32, shuffle=False)
+
+# Now you can use `subset_loader` to process the images
+# Assuming `model` is your pre-loaded model
+model.eval()  # Set the model to evaluation mode
+
+
+def get_activation(name):
+    def hook(model, input, output):
+        print(f"Hook activated for {name}")
+        activation[name] = output.detach()
+        print(f"Output shape: {output.detach().shape}")
+    return hook
+
+activation = {}
+
+layer = model.stages[2][0].norm
+layer_string='stages.2.0.norm'
+
+hook = layer.register_forward_hook(get_activation(layer_string))
+
+features = []
+labelsV=[]
+with torch.no_grad():
+    for inputs, labels in subset_loader:
+        inputs = inputs.cuda()
+        
+        outputs = model(inputs)  # Extract features
+        featuresX = activation[layer_string]
+        features.extend(featuresX.cpu().numpy())
+        labelsV.extend(labels)
+     
+ 
+feat_array=np.array(features);
+mdic = {"feat_array": feat_array, 'labels':labelsV}
+np.save('feature_matrix_s1norm1_val.npy', feat_array)
+
+hook.remove()
+
+activation = {}
+
+layer = model.stages[2][2].norm
+layer_string='stages.2.2.norm'
+
+hook = layer.register_forward_hook(get_activation(layer_string))
+
+features = []
+labelsV=[]
+with torch.no_grad():
+    for inputs, labels in subset_loader:
+        inputs = inputs.cuda()
+        
+        outputs = model(inputs)  # Extract features
+        featuresX = activation[layer_string]
+        features.extend(featuresX.cpu().numpy())
+        labelsV.extend(labels)
+        
+feat_array=np.array(features);
+mdic = {"feat_array": feat_array, 'labels':labelsV}
+
+np.save('feature_matrix_s1norm2_val.npy', feat_array)
+
+hook.remove()
